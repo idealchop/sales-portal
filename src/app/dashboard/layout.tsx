c3c2b2ed7b107d61ed546c143aa50afc33f72f79
Jimboy Regalado <jimbs.work@gmail.com>
2025-10-31 12:47:20 +0000

    This is good but looks the content is cropped. (_for element img_)

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -43,9 +43,7 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
       <div className="flex flex-col flex-1">
         <DashboardHeader />
         <main className="flex-1 overflow-auto p-4 sm:p-6 lg:p-8">
-            <div className="mx-auto max-w-7xl">
-                {children}
-            </div>
+            {children}
         </main>
       </div>
     </SidebarProvider>
