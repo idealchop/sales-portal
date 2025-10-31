85ae2e738f68a59bbb6422e871dbbe51d73f5aeb
Jimboy Regalado <jimbs.work@gmail.com>
2025-10-31 11:32:34 +0000

    IN the image. Would you be able to totoal place the image at the buttom

--- src/app/dashboard/proposals/new/page.tsx
+++ src/app/dashboard/proposals/new/page.tsx
@@ -42,7 +42,7 @@ function InputField({
 
 export default function NewProposalPage() {
   return (
-    <div className="relative flex min-h-[calc(100vh-10rem)] flex-col justify-between">
+    <div className="relative flex flex-col justify-between">
       <div className="flex flex-col gap-6">
         <div className="flex items-center justify-between">
           <div>
