ba65ab3d7fc920f7fce3dd93cc4dfc5e8f976ed5
Jimboy Regalado <jimbs.work@gmail.com>
2025-11-01 17:12:51 +0000

    Update text. It should be Create Proposal (_for element <LinkComponent>_

--- src/app/dashboard/proposals/page.tsx
+++ src/app/dashboard/proposals/page.tsx
@@ -224,7 +224,7 @@ export default function ProposalsPage() {
           <Link href="/dashboard/proposals/new">
             <PlusCircle className="h-3.5 w-3.5" />
             <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-              New Proposal
+              Create Proposal
             </span>
           </Link>
         </Button>
