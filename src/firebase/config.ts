d7c37138c50455e222e5a854250daa4396ce43f5
Jimboy Regalado <jimbs.work@gmail.com>
2025-11-04 12:32:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9213942402-eacce",
+  "appId": "1:787787045039:web:cd7eeee7fd0438f496dc5a",
+  "apiKey": "AIzaSyBTruenLaGWlgTW69Cfy-6cotDmWc0ulCY",
+  "authDomain": "studio-9213942402-eacce.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "787787045039"
+};
