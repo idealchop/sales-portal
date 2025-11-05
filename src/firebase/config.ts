5212865ae0db5879cd703def9e86f689f3ed4154
Jimboy Regalado <jimbs.work@gmail.com>
2025-11-05 05:03:32 +0000

    Console Error
    FirebaseError: Firebase Storage: No default bucket found

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-9213942402-eacce",
   "appId": "1:787787045039:web:cd7eeee7fd0438f496dc5a",
+  "storageBucket": "studio-9213942402-eacce.appspot.com",
   "apiKey": "AIzaSyBTruenLaGWlgTW69Cfy-6cotDmWc0ulCY",
   "authDomain": "studio-9213942402-eacce.firebaseapp.com",
   "measurementId": "",
