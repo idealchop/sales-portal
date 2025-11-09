3c6811ec663839417d992e266bb871d6533ad68f
Jimboy Regalado <jimbs.work@gmail.com>
2025-11-09 04:03:05 +0000

    we need to make it clear. We have 2 type of comissions. 1st - one time c

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -380,7 +380,7 @@ export default function DashboardPage() {
                 <DialogTrigger asChild>
                     <div className="cursor-pointer p-6">
                         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2 p-0">
-                            <CardTitle className="text-sm font-medium">Monthly Commission</CardTitle>
+                            <CardTitle className="text-sm font-medium">One-Time Commission</CardTitle>
                             <CircleDollarSign className="h-4 w-4 text-primary-foreground/80" />
                         </CardHeader>
                         <CardContent className="p-0 pt-2">
@@ -1003,5 +1003,6 @@ export default function DashboardPage() {
     </div>
   );
 }
+
 
 
